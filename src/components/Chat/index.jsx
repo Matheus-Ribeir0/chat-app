2e3a3f2d09c654dfda3f4326030206c001a132ec
Matheus Ribeiro <matheus.tex02@gmail.com>
2023-06-19 21:43:42 -0300

    criado array para separar mensagens de destinários diferente e agrupa-lás

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -11,33 +11,35 @@ import { useRef } from 'react'
 
 
 export const Chat = (props) => {
+
     const dummy = useRef()
     const messageRef = collection(databaseApp, "messages")
-    const QueryMessages = query(messageRef, orderBy("createdAt"),limit(25))//faz uma querry para organizar as menssagens por ordem de envio
+    const QueryMessages = query(messageRef, orderBy("createdAt", "asc"),limit(25))
+
+    
+//faz uma querry para organizar as menssagens por ordem de envio
     const [messages] = useCollectionData(QueryMessages, {idField: "id"})
 
     const sendMessage = async (menssagem)  => {
         const { photoURL, uid } = auth.currentUser
-        console.log(uid, menssagem)
-        debugger
         await addDoc(messageRef , {
             text: menssagem,
             uid,
             photoURL,
             createdAt: serverTimestamp()
         })
+
         dummy.current.scrollIntoView({behavior : "smooth"})
     }
-
+ 
     return (
         <>
         <div className='chat'>
-            {messages && messages.map(msg=> (<Message key={msg.text} message={msg.text} image={msg.photoURL}></Message>))}
+            <Message messages={messages} ></Message>
             <div ref={dummy}></div>
         </div>
         <Form sendMessage={menssagem => sendMessage(menssagem)}/>
         </>
     )
 }
-
-//older {props.listaMenssagens && props.listaMenssagens.map(message => (<Message message={message.text}></Message>))}
+//{messages && messages.map((msg, id) => (<Message key={id} message={msg.text} image={msg.photoURL}></Message>))}
--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,12 +1,50 @@
+import { useEffect, useState } from 'react'
 import './Message.css'
 
 const Message = (props) => {
-    return (
-        <div className='message'>
-            <img src={props.image} alt={"usuário"}></img>
-            <p>{props.message}</p>
-        </div>
-    )
-}
 
+    const messageGroups = [];
+    if (props.messages) {
+        // Agrupa as mensagens consecutivas do mesmo remetente
+        for (let i = 0; i < props.messages.length; i++) {
+            const currentMessage = props.messages[i];
+            const previousMessage = props.messages[i - 1];
+
+            if (previousMessage && previousMessage.uid === currentMessage.uid) {
+                // Adiciona a mensagem atual ao grupo existente
+                messageGroups[messageGroups.length - 1].push(currentMessage);
+            } else {
+                // Cria um novo grupo com a mensagem atual
+                messageGroups.push([currentMessage]);
+            }
+        }
+    }
+    console.log(messageGroups)
+
+    return (
+        <div className="message">
+
+            {messageGroups.map((messageGroup, index) => {
+                const lastMessage = messageGroup[messageGroup.length - 1];
+               
+                return (
+                    <div className="message-group" key={messageGroup.uid}>
+
+                        {messageGroup.map((msg, id) => (
+                            <div className="message-list" key={id}>
+                                {id === messageGroup.length - 1 &&(
+                                    <img src={lastMessage.photoURL || 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png'} alt={"usuário"}></img>
+                                )}
+
+                                <p className='message-text'>{msg.text}</p>
+                            </div>
+                        ))
+                        }
+
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
 export default Message
